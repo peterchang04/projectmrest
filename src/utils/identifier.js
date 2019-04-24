cc471eb680d91a94171aec1de25533207c1b0556
Peter <peter.chang.04@gmail.com>
2019-04-24 08:48:12 -0700

    new identifiers

--- src/utils/identifier.js
+++ src/utils/identifier.js
@@ -1,17 +1,36 @@
 const identifierTypes = [
-  "%count% %adverb% %verb%ing %noun%",
-  "%adverb% %verb%ing %count% %noun%",
-  "%count% %verb%ing %noun%",
-  "%verb%ing %count% %noun%",
-  "%adverb% %verb%ing %noun%",
-  "%adverb% %verb% %count% %noun%",
-  "%verb% %count% %noun%",
-  "%verb%ing %noun%",
-  "%verb% %noun%"
+  //"%count% %adverb% %verb%ing %noun%",
+  //"%adverb% %verb%ing %count% %noun%",
+  //"%count% %verb%ing %noun%",
+  //"%verb%ing %count% %noun%",
+  //"%adverb% %verb%ing %noun%",
+  //"%adverb% %verb% %count% %noun%",
+  //"%verb% %count% %noun%",
+  //"%verb%ing %noun%",
+  //"%verb% %noun%",
+  "%adjective% %noun% %count%",
+  "%adjective% %noun%",
 ];
 // short words best
-const nouns = [ // plural
-  'llamas', 'cats', 'toads', 'cupcakes', 'knights', 'ants', 'hippos', 'feet'
+const nouns = [
+  // animals
+  'llama', 'cat', 'toad', 'knight', 'ant', 'hippo', 'bovine', 'kitty', 'lizard',
+  'beetle', 'bug', 'panther', 'cheetah', 'lion', 'tiger', 'horse', 'moose', 'raptor', 'dinosaur',
+  'slug', 'snail', 'squid', 'fish', 'shark', 'whale', 'orca', 'gerbil', 'rat', 'hamster', 'dog',
+  'bulldog', 'husky', 'terrier', 'retriever', 'pup', 'puppy',
+  // food items
+  'cupcake', 'cake', 'pie', 'pizza', 'burger', 'sandwich', 'roast', 'nugget', 'apple', 'grape',
+  'berry', 'seed',
+  // body parts
+  'foot', 'tooth', 'finger', 'toe', 'ear', 'nose', 'armpit',
+  // places
+  'valley', 'kitchen', 'bathtub', 'tub', 'yard', 'hill', 'forest', 'river',
+  // vehicles
+  'boat', 'sled', 'car', 'bike', 'skateboard', 'snowboard', 'moped',
+  // other
+  'basket', 'shoe', 'shirt', 'skirt',
+  // plants
+  'tree', 'fruit', 'bush',
 ];
 const verbs = [ // must work as [verb]ing e.g. fucking. Stick w/ physical
   'eat', 'kick', 'kiss', 'push', 'lick', 'pull', 'color', 'throw', 'chew'
@@ -19,19 +38,34 @@ const verbs = [ // must work as [verb]ing e.g. fucking. Stick w/ physical
 const adverbs = [
   'eagerly', 'slowly', 'weirdly', 'kindly', 'slyly', 'firmly', 'quickly'
 ];
+const adjectives = [
+  // texture
+  'smooth', 'rough', 'jagged', 'sharp', 'slippery', 'textured',
+  // shape
+  'square', 'oval', 'round',
+  // colors
+  'yellow', 'green', 'red', 'crimson', 'black', 'dark', 'bright', 'white', 'cream',
+  'orange', 'blue', 'navy', 'purple', 'pink',
+  // other
+  'albino', 'greasy', 'lazy', 'crazy', 'creepy', 'scary', 'lovely', 'funny',
+  // size
+  'tiny', 'small', 'fat', 'skinny', 'large', 'huge', 'medium', 'normal',
+];
 
 function generate(min = 5, max = 99) {
   const randomType = random(0, identifierTypes.length - 1);
   const randomVerb = random(0, verbs.length - 1);
   const randomNoun = random(0, nouns.length - 1);
-  const randomAdverb = random(0, adverbs.length -1);
+  const randomAdverb = random(0, adverbs.length - 1);
+  const randomAdj = random(0, adjectives.length - 1);
   const randomCount = random(min, max);
 
   let randomIdentifier = identifierTypes[randomType];
   randomIdentifier = randomIdentifier.replace('%count%', randomCount);
   randomIdentifier = randomIdentifier.replace('%noun%', nouns[randomNoun]);
-  randomIdentifier = randomIdentifier.replace('%verb%', verbs[randomVerb]);
-  randomIdentifier = randomIdentifier.replace('%adverb%', adverbs[randomAdverb]);
+  randomIdentifier = randomIdentifier.replace('%adjective%', adjectives[randomAdj]);
+  //randomIdentifier = randomIdentifier.replace('%verb%', verbs[randomVerb]);
+  //randomIdentifier = randomIdentifier.replace('%adverb%', adverbs[randomAdverb]);
 
   return randomIdentifier;
 }

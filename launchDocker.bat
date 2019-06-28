f07219d2528ae60f1eccddf91ae46a30f2fc7cdc
peter chang <peter.chang.04@gmail.com>
2019-06-27 23:04:47 -0700

    fix launchDocker to pull latest image before launching

--- launchDocker.bat
+++ launchDocker.bat
@@ -1,2 +1,3 @@
 cd C:/projectmrest
-docker run -it -v %cd%:/app -p 51337:51337 gcr.io/projectm-rest/local:latest
+docker pull gcr.io/projectm-rest/local:latest
+docker run -it -v %cd%:/app -p 51337:51337 --name projectmrest_local gcr.io/projectm-rest/local:latest
